944a7b6534debd8ced37ff20a6482cf8df02b3ad
GrxmmLaw <114615472+GrxmmLaw@users.noreply.github.com>
2023-06-24 21:38:56 -0400

    Reescritura y reorganizacion del codigo

---
+++ Mini2/QuadTree.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "utilities.h"
+using namespace std;
+
+class QuadTree{
+    private:
+        Point leftU, rightL;
+        City ciudad;
+        QuadTree* Quad1;
+        QuadTree* Quad2;
+        QuadTree* Quad3;
+        QuadTree* Quad4;
+        bool type;
+    public:
+        QuadTree(Point, Point);
+        ~QuadTree();
+        int totalPoints();
+        int totalNodes();
+        void insert(Point, City);
+        vector<City>* search(QuadTree*, vector<City>*);
+        vector<City>* list();
+        int countRegion(Point, int);
+        int aggregateRegion(Point, int);
+};
